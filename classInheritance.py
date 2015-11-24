8df77142c072458ae415a978dc2623213bad349e
James-Jin <jamesjin37@gmail.com>
2015-11-24 00:01:19 -0500

    add classinheritance.py

---
+++ classInheritance.py
@@ -0,0 +1,34 @@
+class Parent:
+	parentAttr = 100
+	def __init__(self):
+		print "calling  parent constructor"
+		
+	def parentMethod(self):
+		print "calling parent Method"
+		
+	def setAttr(self, attr):
+		Parent.parentAttr = attr
+		
+	def getAttr(self):
+		print "parent attributes =",Parent.parentAttr
+		
+	def commonMethod(self):
+		print "calling parent common Method"
+
+class Child(Parent):
+	def __init__(self):
+		print "calling child constructor"
+	
+	def childMethod(self):
+		print "Calling child method"
+		
+	def commonMethod(self):
+		print "Child common method. Override!"
+		
+		
+c = Child()
+c.childMethod()
+c.parentMethod()
+c.setAttr(200)
+c.getAttr()
+c.commonMethod()
